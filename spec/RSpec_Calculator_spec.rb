b27837ff7f868380204b89d9d315fbf33963c80d
Oliver Sharpe <osharpe@spartaglobal.com>
2018-11-20 15:18:17 +0000

    refactored code

--- spec/RSpec_Calculator_spec.rb
+++ spec/RSpec_Calculator_spec.rb
@@ -1,20 +1,19 @@
 require 'spec_helper'
 
 describe 'CalcEngine' do
+  before(:all) do
+    @calc = CalcEngine.new
+  end
   it "should correctly add two numbers" do
-    calc = CalcEngine.new
-    expect(calc.add(1,2)).to eq 3
+    expect(@calc.add(1,2)).to eq 3
   end
   it "should correctly subtract one number from another" do
-    calc = CalcEngine.new
-    expect(calc.subtract(3,1)).to eq 2
+    expect(@calc.subtract(3,1)).to eq 2
   end
   it "should correctly multiply two numbers" do
-    calc = CalcEngine.new
-    expect(calc.multiply(3,2)).to eq 6
+    expect(@calc.multiply(3,2)).to eq 6
   end
   it "should correctly divide two numbers" do
-    calc = CalcEngine.new
-    expect(calc.divide(9,3)).to eq 3
+    expect(@calc.divide(9,3)).to eq 3
   end
 end
